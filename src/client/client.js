6f62fd76e73de7b5906733bd2b048bd428341f07
chaikritt <techaploog@gmail.com>
2023-05-08 16:12:05 +0700

    debug

--- src/client/client.js
+++ src/client/client.js
@@ -15,8 +15,8 @@ const DATA_FREQ = 30000;
 const MAX_WAIT_COUNT = 5;
 
 const INIT_STATE = {
-  waitReplyCount : 0,
-  readyToSend : false,
+  waitReplyCount: 0,
+  readyToSend: false,
 };
 
 const destination = {
@@ -25,41 +25,40 @@ const destination = {
   localPort: Number(process.env.PVMS_CLIENT_LOCAL_PORT),
 };
 
-const clientState = {...INIT_STATE};
-
+const clientState = { ...INIT_STATE };
 
 // * =============
 
 const sendNotification = async (client, resend = false) => {
   clientState.readyToSend = false;
 
-  if (clientState.waitReplyCount > MAX_WAIT_COUNT){
+  if (clientState.waitReplyCount > MAX_WAIT_COUNT) {
     console.log("[ERROR]");
-    console.log(" - NO response from server side.")
+    console.log(" - NO response from server side.");
     console.log(" - Stop sending message.");
-    return ;
-  }else if (clientState.waitReplyCount > 0) {
+    return;
+  } else if (clientState.waitReplyCount > 0) {
     resend = true;
   }
 
   const msg = await getMessageToSend(resend);
-  
+
   if (msg) {
     client.write(msg);
     clientState.waitReplyCount += 1;
-    console.log(`[${resend ? "R-":"  "}SEND] >>`, msg);
+    console.log(`[${resend ? "R-" : "  "}SEND] >>`, msg);
   }
 
   clientState.readyToSend = true;
 };
 
 // Send data every 30 seconds.
-const intervalID = setInterval(()=>{
-  const {readyToSend} = clientState;
-  if (readyToSend){
-      sendNotification(client);
+const intervalID = setInterval(() => {
+  const { readyToSend } = clientState;
+  if (readyToSend) {
+    sendNotification(client);
   }
-},DATA_FREQ);
+}, DATA_FREQ);
 
 // * ==============
 // * Client Algorithms
@@ -73,9 +72,6 @@ client.on("connect", () => {
   resetMessage();
   clientState.waitReplyCount = 0;
   sendNotification(client);
-
-  console.log(client.connect)
-
 });
 
 // when receive reply
@@ -86,50 +82,49 @@ client.on("data", (buffer) => {
   // Reset clientState.waitReplyCount
   clientState.waitReplyCount = 0;
 
-  const {replyCode} = replyObj;
+  const { replyCode } = replyObj;
 
   // ! DEGUB
   console.log(replyObj);
 
-  if (replyCode === "00"){
-    console.log(`[ REPLY] -> ${replyCode} : ${replyObj.replyDesc}.`)
-  } else if (replyCode === "R0"){
-    sendNotification(client,resend=true);
-  }else {
+  if (replyCode === "00") {
+    console.log(`[ REPLY] -> ${replyCode} : ${replyObj.replyDesc}.`);
+  } else if (replyCode === "R0") {
+    sendNotification(client, (resend = true));
+  } else {
     console.log(`[ REPLY] << ${replyCode} : ${replyObj.replyDesc}.`);
-    console.log(' - Disconnected.');
-    console.log(' - Re-Run this service when server side is ready.');
+    console.log(" - Disconnected.");
+    console.log(" - Re-Run this service when server side is ready.");
 
     resetMessage();
-    clientState = {...INIT_STATE};
+    clientState = { ...INIT_STATE };
     clearInterval(intervalID);
   }
-
 });
 
 client.on("close", () => {
   console.log("[ERROR]");
-  console.log(' - Disconnected from server side.');
-  console.log(' - Re-Run this service when server side is ready.')
+  console.log(" - Disconnected from server side.");
+  console.log(" - Re-Run this service when server side is ready.");
   resetMessage();
-  clientState = {...INIT_STATE};
+  clientState = { ...INIT_STATE };
   clearInterval(intervalID);
 });
 
 client.on("error", () => {
   console.log("[ERROR]");
-  if (client.connect){
-    console.log(' - Error from receiver side.');
-    console.log(' - Re-Run this service when server side is ready.');
-  }else{
-    console.log(' - Cannot connect to server.');
-    console.log(' - Re-Run this service when server side is ready.');
+  if (client.closed) {
+    console.log(" - Cannot connect to server.");
+    console.log(" - Re-Run this service when server side is ready.");
+  } else {
+    console.log(" - Error from receiver side.");
+    console.log(" - Re-Run this service when server side is ready.");
   }
-  try{
+  try {
     resetMessage();
-    clientState = {...INIT_STATE};
+    clientState = { ...INIT_STATE };
     clearInterval(intervalID);
-  }catch (err){
-    console.log(err)
+  } catch (err) {
+    console.log(err);
   }
 });
